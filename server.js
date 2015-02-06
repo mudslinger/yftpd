53c4c27e0dc82e04f6011131f5569268ed936c22
Hisato Tanaka <hisato.tanaka@gmail.com>
2015-02-06 17:13:15 +0900

    cwd

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ forever = require('forever');
 forever.start(
   './ftp-service.coffee',
   {
-    uid: 'root',
+    //uid: 'root',
+    cwd: '/srv/www/yftp/current'
     command: './node_modules/.bin/coffee',
     spawnWith: {
-      customFds: [-1, -1, -1], // that forever spawns.
-      setsid: false,
       uid: 0, // Custom UID
       gid: 0  // Custom GID
     }
