0d99d1964c6402df302b270362fa6a60f29afe44
Hisato Tanaka <hisato.tanaka@gmail.com>
2015-02-08 19:59:02 +0900

    forever

--- server.js
+++ server.js
@@ -1,2 +1,21 @@
-require('coffee-script/register');
-require('./ftp-service')
+var forever = require('forever');
+var express = require('express');
+
+var app = express();
+app.set('port', 80);
+app.get('/',function(req, res){
+  res.send("express is running.");
+});
+app.listen(80);
+console.log('Express listening on port 80');
+
+forever.start(
+  'ftp-service.js',
+  {
+    uid: 'root',
+    spawnWith: {
+      uid: 0, // Custom UID
+      gid: 0  // Custom GID
+    }
+  }
+);
